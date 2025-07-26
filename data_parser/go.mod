b1b2ffd2cbcf923e9494ddaed5a1c8d2d2e7e84c
Anthony <antnay@uw.edu>
2025-07-26 11:09:50 -0700

    feat: update parser

--- data_parser/go.mod
+++ data_parser/go.mod
@@ -5,12 +5,16 @@ go 1.24.3
 require github.com/jackc/pgx/v5 v5.7.5
 
 require (
+	github.com/google/uuid v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
+	go.opencensus.io v0.22.3 // indirect
 	golang.org/x/crypto v0.40.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+	googlemaps.github.io/maps v1.7.0 // indirect
 )
--- data_parser/main.go
+++ data_parser/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"data_parser/db"
 	"encoding/csv"
 	"fmt"
@@ -9,11 +10,18 @@ import (
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"googlemaps.github.io/maps"
 )
 
 func main() {
+
 	if _, err := os.ReadFile(".env"); err != nil {
 		log.Fatalln("Need .env in data_parser folder\nwith: POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB!!")
+	}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %s\n", err)
 	}
 	p, err := db.Connect()
 	if err != nil {
@@ -22,15 +30,23 @@ func main() {
 	log.Println("Connected to db!")
 	defer p.Close()
 
+	log.Println(os.Getenv("MAPS_API"))
+	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("MAPS_API")))
+	if err != nil {
+		log.Fatalf("fatal error: %s\n", err)
+	}
+
+	// addNeighborhood(p, c)
+
 	argsWithoutProg := os.Args[1:]
 	if len(argsWithoutProg) == 0 {
 		log.Fatalln("provide a csv!")
 	}
-	readCSV(p, argsWithoutProg[0])
+	readCSV(p, c, argsWithoutProg[0])
 	os.Exit(0)
 }
 
-func readCSV(p *pgxpool.Pool, path string) {
+func readCSV(p *pgxpool.Pool, c *maps.Client, path string) {
 	dat, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("cant read file %s\nerr: %s\n", path, err)
@@ -38,6 +54,7 @@ func readCSV(p *pgxpool.Pool, path string) {
 	defer dat.Close()
 
 	reader := csv.NewReader(dat)
+	count := 0
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
@@ -46,12 +63,183 @@ func readCSV(p *pgxpool.Pool, path string) {
 		if err != nil {
 			log.Fatal(err)
 		}
+		// fmt.Println(record)
 
-		fmt.Println(record)
+		if count != 0 {
+			// log.Println(len(record))
+			insert(p, c, record)
+			// for i := 0; i < 10; i++ {
+			// 	_, err = reader.Read()
+			// 	if err == io.EOF {
+			// 		break
+			// 	}
+			// 	count++
+			// }
+		}
+		log.Println(count)
+		count++
 	}
 	// log.Println(records)
 }
 
-func insert(p *pgxpool.Pool, record []string) {
-	// sql := `insert into `
+func insert(p *pgxpool.Pool, c *maps.Client, record []string) {
+	caseno := record[0]
+	lat := record[1]
+	lon := record[2]
+	category := record[3]
+	address := record[4]
+	date := record[5]
+	time := record[6]
+	neighborhood := record[8]
+
+	// if redacted continue
+	// regex to filter out xxxx block of ...
+	// reorganize categories
+
+	req := maps.GeocodingRequest{Address: fmt.Sprintf("%s, tacoma, WA", address)}
+
+	loc, err := c.Geocode(context.Background(), &req)
+	if err != nil {
+		log.Printf("fatal error: %s\n", err)
+	}
+
+	// log.Println(loc[0].AddressComponents[7])
+	var zip string
+
+	defer func() {
+		if err := recover(); err != nil {
+			log.Println("panic occurred:", err)
+		}
+	}()
+	comp := loc[0].AddressComponents
+	len := len(comp)
+	goodZip := false
+	goodNei := false
+	for i := len - 1; i > 0; i-- {
+		curr := comp[i]
+		if curr.Types[0] == "postal_code" {
+			// log.Println(curr.LongName)
+			zip = curr.LongName
+			goodZip = true
+		}
+		if curr.Types[0] == "neighborhood" {
+			neighborhood = curr.LongName
+			goodNei = true
+		}
+	}
+	if !(goodZip && goodNei) {
+		return
+	}
+
+	sql := `SELECT *
+		FROM add_crime_incident_partition(p_city_name := $1,
+	                              p_state_name := $2,
+	                              p_country_name := $3,
+	                              p_source_name := $4,
+	                              p_latitude := $5,
+	                              p_longitude := $6,
+	                              p_street_address := $7,
+	                              p_postal_code := $8,
+	                              p_crime_category_name :=$9,
+								  p_neighborhood := $13,
+	                              p_incident_date := $10::date,
+	                              p_incident_time := $11::time,
+	                              p_case_num := $12)`
+
+	_, err = p.Exec(context.Background(), sql,
+		"Tacoma", "Washington", "United States", "City of Tacoma Reported Crime (Tacoma)",
+		lat, lon, address, zip, category, date, time, caseno, neighborhood)
+
+	if err != nil {
+		log.Printf("errr: %s", err)
+	}
+
+}
+
+func addNeighborhood(p *pgxpool.Pool, c *maps.Client) {
+	sql := `
+	select street_address, postal_code,neighborhood_id 
+	from addresses
+	where neighborhood_id is null
+	`
+
+	q, err := p.Query(context.Background(), sql)
+	if err != nil {
+		log.Println("uh oh ", err)
+	}
+	for q.Next() {
+		row, err := q.Values()
+		if err != nil {
+			log.Println("uh oh ", err)
+		}
+
+		log.Println(row...)
+		// addr := row[len(row)-4]
+		// log.Println(addr)
+
+		req := maps.GeocodingRequest{Address: fmt.Sprintf("%s, tacoma, WA, %s", row[0], row[1])}
+
+		loc, err := c.Geocode(context.Background(), &req)
+		if err != nil {
+			log.Printf("fatal error: %s\n", err)
+		}
+		log.Println(loc)
+
+		defer func() {
+			if err := recover(); err != nil {
+				log.Println("panic occurred:", err)
+			}
+		}()
+		comp := loc[0].AddressComponents
+		len := len(comp)
+
+		good := false
+		var neighborhood string
+		// log.Println(len)
+		for i := range len {
+			curr := comp[i]
+			// log.Println(curr.Types)
+			if curr.Types[0] == "neighborhood" {
+				neighborhood = curr.LongName
+				log.Println(curr.LongName)
+				good = true
+			}
+		}
+		if !good {
+			continue
+		}
+
+		continue
+
+		sql = `
+		select neighborhood_id from neighborhoods where neighborhood_name = $1
+		`
+		var neighborhoodId int
+		p.QueryRow(context.Background(), sql, neighborhood).Scan(&neighborhoodId)
+
+		if neighborhoodId == 0 {
+			sql = `
+			WITH city_query AS (SELECT city_id
+                    FROM cities
+                    WHERE city_name = 'Tacoma')
+			INSERT
+			INTO neighborhoods (neighborhood_name, city_id)
+			SELECT data.neighborhood_name,
+		   		s.city_id
+			FROM city_query s
+		     	CROSS JOIN(VALUES ($1))
+			AS data(neighborhood_name) 
+			returning neighborhood_id`
+			p.QueryRow(context.Background(), sql, neighborhood).Scan(&neighborhoodId)
+		}
+
+		sql = `
+		UPDATE addresses
+		SET neighborhood_id = $1
+		WHERE street_address = $2 and postal_code = $3
+		`
+		var update []string
+		p.QueryRow(context.Background(), sql, neighborhoodId, row[0], row[1]).Scan(update)
+		log.Println()
+	}
 }
